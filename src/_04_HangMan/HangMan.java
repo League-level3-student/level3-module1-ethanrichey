3cc2fc627b20897eed0917d4a24da1ae3c7e0ebf
KingArman999 <44267696+KingArman999@users.noreply.github.com>
2019-03-02 14:19:32 -0800

    bruh

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -6,6 +6,7 @@ import java.util.Dictionary;
 import java.util.Stack;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
@@ -14,7 +15,9 @@ import org.omg.Messaging.SyncScopeHelper;
 public class HangMan implements KeyListener {
 JFrame frame = new JFrame();
 JPanel panel = new JPanel();
+JLabel label = new JLabel();
 Stack <String> words = new Stack<String>();
+String under = "_";
 	public static void main(String[]args) {
 		HangMan hm = new HangMan();
 		hm.run();
@@ -25,16 +28,31 @@ Stack <String> words = new Stack<String>();
 		frame.setVisible(true);
 		frame.add(panel);
 		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		panel.add(label);
 		String userType = JOptionPane.showInputDialog("How many words do you want to solve?");
 		Integer num = new Integer(userType);
 		for(int i = 0; i < num; i ++) {
 			String word = Utilities.readRandomLineFromFile("dictionary.txt");
 			words.push(word);
+			
 		}
-		System.out.println(words);
-		
+		wordBlanks();
 	}
 
+	public void wordBlanks() {
+		System.out.println(words);
+		String ward = words.pop();
+		String display = ward;
+		label.setText(display);
+		for (int i = 0; i < display.length(); i++) {
+			
+			
+		}
+		
+		
+		
+		
+	}
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
